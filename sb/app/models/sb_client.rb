c3afc2e7f87b6323d98c3b7ad4931cae9f0cc092
m.watanabe <m.watanabe@alarmbox.co.jp>
2021-06-04 09:46:10 +0900

    SB-51 クライアントの業種２はフリーワードなのでマスタ連携から外す、SB-52 クライアントテーブルに資本金を追加する

--- sb/app/models/sb_client.rb
+++ sb/app/models/sb_client.rb
@@ -2,8 +2,7 @@ class SbClient < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture, primary_key: "code", foreign_key: "prefecture_code"
   belongs_to_active_hash :area
-  belongs_to_active_hash :industry1, class_name: "Industry", primary_key: "code", foreign_key: "industry_code1"
-  belongs_to_active_hash :industry2, class_name: "Industry", primary_key: "code", foreign_key: "industry_code2"
+  belongs_to_active_hash :industry, class_name: "Industry", primary_key: "code", foreign_key: "industry_code"
   belongs_to_active_hash :channel
 
   belongs_to :sb_tanto, optional: true, class_name: "InternalUser", foreign_key: "sb_tanto_id"
