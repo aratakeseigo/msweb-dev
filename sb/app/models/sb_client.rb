9a552680791b17eb0a0c9b1ff7cd0ec355b534cc
taira.masahiro <m.taira@alarmbox.co.jp>
2021-06-07 11:05:50 +0900

    SB-38 途中経過修正

--- sb/app/models/sb_client.rb
+++ sb/app/models/sb_client.rb
@@ -5,6 +5,7 @@ class SbClient < ApplicationRecord
   belongs_to_active_hash :industry1, class_name: "Industry", primary_key: "code", foreign_key: "industry_code1"
   belongs_to_active_hash :industry2, class_name: "Industry", primary_key: "code", foreign_key: "industry_code2"
   belongs_to_active_hash :channel
+  belongs_to_active_hash :status
 
   belongs_to :sb_tanto, optional: true, class_name: "InternalUser", foreign_key: "sb_tanto_id"
   has_many :sb_client_user
