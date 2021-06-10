5d5da7052b7ae81249467bde76846dfb1f836cd9
m.watanabe <m.watanabe@alarmbox.co.jp>
2021-06-10 11:29:24 +0900

    SB-62 【CL登録】法人番号で特定できた既存企業に割り当てる 一旦コミット

--- sb/app/models/entity.rb
+++ sb/app/models/entity.rb
@@ -2,4 +2,19 @@ class Entity < ActiveRecord::Base
   # 履歴は別テーブルの予定なので、profileは単体
   has_one :entity_profile
   accepts_nested_attributes_for :entity_profile
+
+
+  def self.assign_entity(company_name,daihyo_name,taxagency_corporate_number,address)
+    address_before_choumei = address
+    recommends = find_by_company_no_and_address(taxagency_corporate_number,address)
+
+  end
+
+  def self.select_by_company_no_and_address(taxagency_corporate_number,address)
+  end
+
+
+  def self.select_by_company_name_and_daihyo_name(company_name,daihyo_name,)
+  end
+
 end
