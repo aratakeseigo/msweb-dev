4f85d0f73a0692720700dd3fcff9b2e3adb5cd1d
m.watanabe <m.watanabe@alarmbox.co.jp>
2021-06-13 00:53:36 +0900

    SB-66 決裁テーブルの設計とクライアントへの組み込み メソッドの場所を移動

--- sb/app/models/concerns/approvable.rb
+++ sb/app/models/concerns/approvable.rb
@@ -1,72 +1,34 @@
 module Approvable
   def apply(user, comment = "", target = self)
-    # 申請が存在しないか、
-    # 申請が存在した場合、
-    #   承認済みならできない
-    #   取り下げ済みもしくは差し戻し済みなら申請できる
-    unless target.approval.nil?
-      if Status::SbApproval::APPROVED == target.approval.status
-        raise ApproveError.new "承認済みです"
-      end
-      unless [Status::SbApproval::WITHDRAWED, Status::SbApproval::REMAND].include? target.approval.status
-        raise ApproveError.new "申請中です"
-      end
+    approval = target.approval
+    if approval.nil?
+      approval = target.build_approval
+      approval.relation_id = target.id
+      approval.apply(user, comment)
+    else
+      target.approval = approval.re_apply(user, comment)
     end
-    approval = target.build_approval
-    approval.applied_user = user
-    approval.applied_at = Time.zone.now
-    approval.apply_comment = comment
-    approval.created_user = user
-    approval.updated_user = user
-    approval.status = Status::SbApproval::APLYING
-    approval.relation_id = target.id
+    target.save!
   end
 
   def withdraw(user, comment = "", target = self)
-    # 申請中でない場合は取り下げできない
-    # 申請者しか取り下げできない
-    approval = target.approval
     raise ApproveError.new "未申請です" if approval.nil?
-    raise ApproveError.new "申請中ではありません" unless approval.status == Status::SbApproval::APLYING
-    raise ApproveError.new "申請者しか取り下げできません" unless approval.applied_user == user
     approval = target.approval
-    approval.approve_comment = comment
-    approval.updated_user = user
-    approval.status = Status::SbApproval::WITHDRAWED
+    approval.withdraw(user, comment)
     approval.save!
   end
 
-  def approve(user, comment = "", target = self, &_)
-    # 申請中でない場合は承認できない
-    # 申請者は承認できない
-    approval = target.approval
+  def approve(user, comment = "", target = self)
     raise ApproveError.new "未申請です" if approval.nil?
-    raise ApproveError.new "申請中ではありません" unless approval.status == Status::SbApproval::APLYING
-    raise ApproveError.new "申請者は承認できません" unless approval.applied_user != user
-    if block_given?
-      raise ApproveError.new "権限がありません" unless yield
-    end
-    approval.approved_at = Time.zone.now
-    approval.approved_user = user
-    approval.approve_comment = comment
-    approval.updated_user = user
-    approval.status = Status::SbApproval::APPROVED
+    approval = target.approval
+    approval.approve(user, comment)
     approval.save!
   end
 
   def remand(user, comment = "", target = self)
-    # 申請中でない場合は承認できない
-    # 申請者は承認できない
-    approval = target.approval
     raise ApproveError.new "未申請です" if approval.nil?
-    raise ApproveError.new "申請中ではありません" unless approval.status == Status::SbApproval::APLYING
-    raise ApproveError.new "申請者は差し戻しできません" unless approval.applied_user != user
-
-    approval.approved_at = Time.zone.now
-    approval.approved_user = user
-    approval.approve_comment = comment
-    approval.updated_user = user
-    approval.status = Status::SbApproval::REMAND
+    approval = target.approval
+    approval.remand(user, comment)
     approval.save!
   end
 
