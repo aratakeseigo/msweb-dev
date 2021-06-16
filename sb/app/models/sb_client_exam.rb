60331319b2c29cf305f2be9fce053f7a64873288
m.watanabe <m.watanabe@alarmbox.co.jp>
2021-06-16 11:33:37 +0900

    SB-91 クライアントに持っている決裁IDを削除して実装を引っ越す リレーション漏れ追加

--- sb/app/models/sb_client_exam.rb
+++ sb/app/models/sb_client_exam.rb
@@ -1,6 +1,7 @@
 class SbClientExam < ApplicationRecord
   include Approvable
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :sb_client
 
   belongs_to :sb_approval, optional: true, class_name: "SbApproval::ClientExam"
   belongs_to :created_user, optional: true, class_name: "InternalUser", foreign_key: "created_by"
