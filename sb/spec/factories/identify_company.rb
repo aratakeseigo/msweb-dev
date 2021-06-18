2909bb206cadc1fcf875985d41f73c2de83d29fb
aratakeseigo <aratake.s@nds-tyo.co.jp>
2021-06-18 19:15:10 +0900

    SB-82,83,84のテストコード実装

---
+++ sb/spec/factories/identify_company.rb
@@ -0,0 +1,126 @@
+FactoryBot.define do
+  factory :identify_company_sb_client1, class: SbClient do
+    transient do
+      entity { build :entity }
+      sb_agent { create :sb_agent }
+      internal_user { build :internal_user, name: "SB担当者１太郎", email: "sb_client_1@example.com" }
+    end
+
+    id { 1 }
+    entity_id { nil }
+    status_id { 1 }
+    taxagency_corporate_number { 1111111111111 }
+    name { "未特定企業１"}
+    daihyo_name { "企業未特定　太郎１" }
+    created_at { "2021/06/01 12:00:00" }
+    updated_at { "2021/06/01 12:00:00" }
+    created_user { internal_user }
+    updated_user { internal_user }
+
+  end
+
+  factory :identify_company_sb_client2, class: SbClient do
+    transient do
+      entity { build :entity }
+      sb_agent { create :sb_agent }
+      internal_user { build :internal_user, name: "SB担当者１太郎", email: "sb_client_1@example.com" }
+    end
+
+    id { 2 }
+    entity_id { nil }
+    status_id { 1 }
+    taxagency_corporate_number { 2222222222222 }
+    name { "未特定企業２"}
+    daihyo_name { "企業未特定　太郎２" }
+    created_at { "2021/06/01 12:00:00" }
+    updated_at { "2021/06/01 12:00:00" }
+    created_user { internal_user }
+    updated_user { internal_user }
+  end
+
+  factory :identify_company_sb_client3, class: SbClient do
+    transient do
+      entity { build :entity }
+      sb_agent { create :sb_agent }
+      internal_user { build :internal_user, name: "SB担当者１太郎", email: "sb_client_1@example.com" }
+    end
+
+    id { 3 }
+    entity_id { nil }
+    status_id { 1 }
+    taxagency_corporate_number { 3333333333333 }
+    name { "未特定企業３"}
+    daihyo_name { "企業未特定　太郎３" }
+    created_at { "2021/06/01 12:00:00" }
+    updated_at { "2021/06/01 12:00:00" }
+    created_user { internal_user }
+    updated_user { internal_user }
+  end
+
+  factory :identify_company_sb_client4, class: SbClient do
+    transient do
+      entity { build :entity }
+      sb_agent { create :sb_agent }
+      internal_user { build :internal_user, name: "SB担当者１太郎", email: "sb_client_1@example.com" }
+    end
+
+    id { 4 }
+    entity_id { nil }
+    status_id { 1 }
+    taxagency_corporate_number { 4444444444444 }
+    name { "未特定企業４"}
+    daihyo_name { "企業未特定　太郎４" }
+    created_at { "2021/06/01 12:00:00" }
+    updated_at { "2021/06/01 12:00:00" }
+    created_user { internal_user }
+    updated_user { internal_user }
+  end
+
+  factory :identify_company_entity1, class: Entity do
+    id { 1111}
+    corporation_number { 1111111111111 }
+    created_at { "2021/06/01 12:00:00" }
+    after(:build) do |entity|
+      entity.entity_profile = FactoryBot.build(:identify_company_entity_profile1)
+    end
+  end
+  factory :identify_company_entity_profile1, class: EntityProfile  do
+    entity_id { 1111 }
+  end
+
+  factory :identify_company_entity2, class: Entity do
+    id { 2222 }
+    created_at { "2021/06/01 12:00:00" }
+    after(:build) do |entity|
+      entity.entity_profile = FactoryBot.build(:identify_company_entity_profile2)
+    end
+  end
+  factory :identify_company_entity_profile2, class: EntityProfile  do
+    entity_id { 2222 }
+    corporation_name { "未特定企業２" }
+  end
+
+  factory :identify_company_entity3, class: Entity do
+    id { 3333 }
+    created_at { "2021/06/01 12:00:00" }
+    after(:build) do |entity|
+      entity.entity_profile = FactoryBot.build(:identify_company_entity_profile3)
+    end
+  end
+  factory :identify_company_entity_profile3, class: EntityProfile  do
+    entity_id { 3333 }
+    daihyo_name { "企業未特定　太郎３" }
+  end
+
+  factory :identify_company_entity4, class: Entity do
+    id { 4444 }
+    created_at { "2021/06/01 12:00:00" }
+    after(:build) do |entity|
+      entity.entity_profile = FactoryBot.build(:identify_company_entity_profile4)
+    end
+  end
+  factory :identify_company_entity_profile4, class: EntityProfile  do
+    entity_id { 4444 }
+  end
+
+end
